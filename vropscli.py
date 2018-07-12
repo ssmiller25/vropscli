f9c3ab4ad499c3f394da76f02e19ba050190a0e8
Steve Miller <steven.miller@bluemedora.com>
2018-07-12 14:44:47 -0400

    Update to explicitly include sys and call

--- vropscli.py
+++ vropscli.py
@@ -5,6 +5,7 @@ import json
 import pprint
 import fire
 import csv
+import sys
 
 
 class vropscli:
@@ -382,23 +383,23 @@ class vropscli:
         #There is the option for it to be UNKNOWN and it isn't accounted for
         if resourceState == "NOT_EXISTING":
             print("The adapter is powered off")
-            exit(1)
+            sys.exit(1)
         elif resourceState == "STARTED":
             #Get the adapters resource status, hopefully data_receiving
             resourceStatus = (json.loads(resources.text)["resourceList"][0]["resourceStatusStates"][0]["resourceStatus"])
             #if the resourceStatus is DATA_RECEIVING let the user know they are collecting data
             if resourceStatus == "DATA_RECEIVING":
                 print("The adapter is on and collecting succesfully")
-                exit(0)
+                sys.exit(0)
             elif resourceStatus == "NO_PARENT_MONITORING":
                 print("The adapter is powered off")
-                exit(1)
+                sys.exit(1)
             else:
                 print("The adapter in on, but not collecting.  Status is " + resourceStatus)
-                exit(1)
+                sys.exit(1)
         else:
             print("Unknown adapter state: " + resourceState)
-            exit(1)
+            sys.exit(1)
 
     def stopAdapterInstance(self, adapterId):
         #set the url for the adapter instance
