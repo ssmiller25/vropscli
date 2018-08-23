a4d636f61c8b5ea65ab1e7fb9ce69265b9ba5f48
Steve Miller <steven.miller@bluemedora.com>
2018-08-23 12:06:48 -0400

    Update example adapter rename script to except parameters

--- examples/renamealerts.sh
+++ examples/renamealerts.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 #
 # Basic script to rename all alerts of a particular type
+#
+if [[ $# -lt 2 ]]
+then
+  echo ""
+  echo  "Error:    Not enough parameters"
+  echo  "Usage:    renamealerts.sh <ADAPTERKIND> <PREPEND_TEXT>"
+  echo  "Example:  /renamealerts.sh PALOALTONETWORKS_ADAPTER VROPS_PA_"
+  echo  "  Note:  The prepended text does NOT include a space automatically"
+  echo "" 
+  echo  "To get Available ADAPTERKINDs, run ./vropscli getAdapterKinds"
+  echo  ""
+  exit 1
+fi
+
+ADAPTERKIND=$1
+PREPENDTEXT=$2
+
 export COUNT=1
-./vropscli getAlertsDefinitionsByAdapterKind HPE3PAR_ADAPTER | python -m json.tool > hpalert.json
-cat hpalert.json | while read line; do 
+./vropscli getAlertsDefinitionsByAdapterKind ${ADAPTERKIND} | python -m json.tool > oldalert.json
+cat oldalert.json | while read line; do 
     if echo $line | grep -q name; then 
         COUNTSTR=$(printf "%03d" $COUNT)
         echo ${line} | sed "s/\"name\": \"/\"name\": \"VROPS_HPE_${COUNTSTR} /g"
@@ -11,5 +28,7 @@ cat hpalert.json | while read line; do
     else 
         echo ${line}
     fi
-done > newhpalertdefs.json
-./vropscli updateAlertDefinitions newhpalertdefs.json
+done > newalert.json
+./vropscli updateAlertDefinitions newalert.json
+rm oldalert.json
+rm newalert.json
