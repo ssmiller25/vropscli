2f6a72f1ca777271c20aa78b795b69cb42b22c09
Joseph Wan <josephwan@jwan-mbp.bluemedora.localnet>
2019-06-05 12:51:55 -0400

    Adding support for linux(ubuntu) build system

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+# Supports vrops and linux(ubuntu)
+
+if [ $1 == "vrops" ]
+then
+    PLATFORM="vrops"
+elif [ $1 == "linux" ]
+then
+    PLATFORM="linux"
+else
+    echo "Failed to get operating system"
+    exit 1
+fi
 
 VERSION=`cat vropscli.py | grep 'VERSION=' | cut -b 9- | tr -d '"'`
 if [ -z "$VERSION" ]
@@ -12,12 +24,14 @@ fi
 mkdir -p artifacts
 
 docker build \
+    -f Dockerfile.${PLATFORM} \
     --no-cache \
     -t vropscli:vrops-${VERSION} .
 
 docker create -ti --name artifacts vropscli:vrops-${VERSION} bash && \
-    docker cp artifacts:/vropscli/dist/vropscli artifacts/vropscli_vrops_v${VERSION}
+    docker cp artifacts:/vropscli/dist/vropscli artifacts/vropscli_${PLATFORM}_v${VERSION}
 
 # cleanup
 docker rm -fv artifacts &> /dev/null
 docker rmi vropscli:vrops-${VERSION}
+
