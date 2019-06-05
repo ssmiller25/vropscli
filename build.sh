183fdff3612dcd3c0c6bd55d5a1e61118f76d338
Joseph Wan <josephwan@jwan-mbp.bluemedora.localnet>
2019-06-05 14:01:42 -0400

    Build for linux(vrops and general linux) is combined

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-# Supports vrops and linux(ubuntu)
-
-if [ $1 == "vrops" ]
-then
-    PLATFORM="vrops"
-elif [ $1 == "linux" ]
-then
-    PLATFORM="linux"
-else
-    echo "Failed to get operating system"
-    exit 1
-fi
+# Supports all linux (includs vrops)
 
 VERSION=`cat vropscli.py | grep 'VERSION=' | cut -b 9- | tr -d '"'`
 if [ -z "$VERSION" ]
@@ -24,14 +13,12 @@ fi
 mkdir -p artifacts
 
 docker build \
-    -f Dockerfile.${PLATFORM} \
     --no-cache \
-    -t vropscli:vrops-${VERSION} .
+    -t vropscli:linux-${VERSION} .
 
-docker create -ti --name artifacts vropscli:vrops-${VERSION} bash && \
-    docker cp artifacts:/vropscli/dist/vropscli artifacts/vropscli_${PLATFORM}_v${VERSION}
+docker create -ti --name artifacts vropscli:linux-${VERSION} bash && \
+    docker cp artifacts:/vropscli/dist/vropscli artifacts/vropscli_linux_v${VERSION}
 
 # cleanup
 docker rm -fv artifacts &> /dev/null
-docker rmi vropscli:vrops-${VERSION}
-
+docker rmi vropscli:linux-${VERSION}
