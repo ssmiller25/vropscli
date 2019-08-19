20b3bc833f34c25a2872746d919f208de9f415e1
Steve Miller <steven.miller@bluemedora.com>
2019-08-19 15:45:35 -0400

    Backup scripts for vrops

--- examples/backupvropsconfig.ps1
+++ examples/backupvropsconfig.ps1
@@ -1,6 +1,7 @@
 #
+#  backupvropsconfig.ps1
 #  A great example, provided by John Xu, that will prompt a user once for credentials, then run multiple instances
-#    of vropscli.  Modified to only prompt if .\
+#    of vropscli.  Modified to only prompt if password file does not exist
 #
 
 cls
@@ -12,12 +13,23 @@ if (Test-Path $env:USERPROFILE\.vropscli.yml) {
     $user = "admin"
     $password = read-host "type into admin password"
     $node = read-host "type in vROps Node"
+    $authcmd = "--user $user --password $password --host $node"
 }
 
 $components = ("getVropsLicense","getCurrentActivity","getAllCredentials","getSolution","getAdapters","getAdapterKinds")
 foreach ($component in $components) {
     write-host "----"$component"--------"
-    .\vropscli.exe $component --user $user --password $password --host $node > $component"_"$time.csv
+    .\vropscli.exe $component $authcmd > $component"_"$time.csv
     write-host "----------------"
 }
-Write-host “-------Done------" 
+
+# Pull solution license configuration
+$solutions=import-csv -path getSolution"_"$time.csv
+New-Item -Path . -Name getSolutionLicense"_"$time.csv -ItemType "file" 
+foreach ($solution in $solutions) {
+    write-host "---Solution License for $solution.id-----"
+    $license = & .\vropscli.exe getSolutionLicense $solution.id 
+    Add-Content -Path getSolutionLicense"_"$time.csv -Value "$solution.id,$license"
+}
+
+Write-host "-------Done------" 
